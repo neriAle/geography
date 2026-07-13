a131b9742ceff02690a29803a76c04f236de07ec
Alessandro Neri <a.neri@student.tudelft.nl>
2026-07-13 10:26:32 +0200

    ✨ Created Country Finder game entry point

---
+++ src/components/games/CountryFinderGameEntry.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import GameShell from "../layout/GameShell.vue";
+import CountryFinder from "./CountryFinder.vue";
+</script>
+
+<template>
+  <GameShell>
+    <CountryFinder />
+  </GameShell>
+</template>
