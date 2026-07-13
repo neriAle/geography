956c5397246e627dbabb453e0e9ea9ed4cb75c77
Alessandro Neri <a.neri@student.tudelft.nl>
2026-07-13 12:30:20 +0200

    ✨ Created Flag Guesser game entry point

---
+++ src/components/games/FlagGameEntry.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import GameShell from "../layout/GameShell.vue";
+import FlagGuesser from "./FlagGuesser.vue";
+</script>
+
+<template>
+  <GameShell>
+    <FlagGuesser />
+  </GameShell>
+</template>
